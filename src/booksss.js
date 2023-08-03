7084084574eeb382c886d169700a861006d6e41a
AARTI SHARMA <120464099+aartisharma05@users.noreply.github.com>
2023-08-03 11:21:01 +0530

    booksss.js

---
+++ src/booksss.js
@@ -0,0 +1,23 @@
+
+ export const data =[
+{ id: 1,
+  img : 'https://m.media-amazon.com/images/I/91hIsjFuICL._AC_UY327_FMwebp_QL65_.jpg',
+  title: 'I love you to the moon and back.' ,
+  author : 'Aarti Sharma'},
+  { id:2,
+    img : 'https://m.media-amazon.com/images/I/811wuvCKC2S._AC_UY327_FMwebp_QL65_.jpg',
+    title: 'The Power of our subconcious mind' ,
+    author : 'Dr. Joseph Murphy'
+  },
+ {  id:3,
+    img : 'https://m.media-amazon.com/images/I/81ZI8RyyynL._AC_UY327_FMwebp_QL65_.jpg',
+    title: 'The Hidden Hindu',
+    author: 'Anish Kumar'
+},
+{   id:4,
+    img : 'https://m.media-amazon.com/images/I/81T05w0B3lL._AC_UY327_FMwebp_QL65_.jpg',
+    title: 'You Can',
+    author: 'George Matthew'
+}
+
+];
